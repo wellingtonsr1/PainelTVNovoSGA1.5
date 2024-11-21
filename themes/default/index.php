3c710e771e22a4f8e40940d08cddc09eca98801c
wellingtonsr1 <97860975+wellingtonsr1@users.noreply.github.com>
2024-11-21 12:44:24 -0300

    Add files via upload

--- themes/default/index.php
+++ themes/default/index.php
@@ -31,8 +31,8 @@
         case 'local':
             ?>
             <!--<iframe id="slides-container"  scrolling="no" src="themes/default/videos/index.php" frameborder="0" ></iframe>-->
-            <video width="1335" height="800" autoplay controls loop>
-                <source src="themes/default/videos/cartilha.mp4" type="video/mp4">
+            <video id="myvideo" width="100%" height="auto" controls="false" autoplay muted loop>
+                <source src="themes/default/videos/cartilha2.mp4" type="video/mp4">
                 Seu navegador não suporta a reprodução de vídeo.
             </video>
             <?php
